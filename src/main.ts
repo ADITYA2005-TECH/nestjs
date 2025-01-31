a8da5fb6ecc80f31430f8328571400f694db8528
Durga Prasad Upadhaya <durga.prasad@aqilea.com>
2025-01-31 18:36:44 +0530

    31010636

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './common/interceptor/logging.interceptor';
 import { ValidationPipe } from '@nestjs/common';
+import { GlobalExceptionFilter } from './common/filter/global.exception';
 
 async function start() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   // app.useGlobalInterceptors(new LoggingInterceptor)
-  
-   
+  // app.useGlobalFilters(new GlobalExceptionFilter);   
   await app.listen(3000);
 }
 
